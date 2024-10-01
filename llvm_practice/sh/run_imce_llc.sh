baca621a85962c44d783042e59bd127747343767
jaeyongjang@mini <jong166@gmail.com>
2024-10-01 00:42:41 +0000

    update imce llc run script ea Please enter the commit message for your changes. Lines starting

--- llvm_practice/sh/run_imce_llc.sh
+++ llvm_practice/sh/run_imce_llc.sh
@@ -5,5 +5,7 @@ filename_no_ext=$(echo "$basename" | sed 's/\(.*\)\..*/\1/')
 llc --march=IMCE $1 -o ./output/$filename_no_ext.s \
   -force-hardware-loops \
   -force-nested-hardware-loop \
+  -hardware-loop-decrement 1 \
+  -hardware-loop-counter-bitwidth 16 \
   -debug-only=isel
 # llc --march=IMCE $1 -debug -force-hardware-loops -force-nested-hardware-loop
