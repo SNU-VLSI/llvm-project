9321670996b631f12c1ea9ae27bffae196e65e8e
jaeyongjang@mini <jong166@gmail.com>
2024-10-22 09:11:02 +0000

    Succeed no error of llc for conv cpp test

--- llvm_practice/sh/test_imce_clang.sh
+++ llvm_practice/sh/test_imce_clang.sh
@@ -2,10 +2,13 @@ basename=$(basename $1)
 filename_no_ext=$(echo "$basename" | sed 's/\(.*\)\..*/\1/')
 # clang -O3 --target=IMCE -S -emit-llvm $1 -I../test_cpp -o output/$filename_no_ext.ll
 clang -O1 --target=IMCE -S -emit-llvm $1 -I../test_cpp -o output/$filename_no_ext.ll
-opt -passes=mem2reg output/$filename_no_ext.ll -o output/$filename_no_ext.ll
-# llc --march=IMCE output/$filename_no_ext.ll -o ./output/$filename_no_ext.s \
-#   -force-hardware-loops \
-#   -force-nested-hardware-loop \
-#   -hardware-loop-decrement 1 \
-#   -hardware-loop-counter-bitwidth 16 \
-#   -debug-only=isel
+# opt -passes=mem2reg output/$filename_no_ext.ll -o output/$filename_no_ext.ll
+llc --march=IMCE output/$filename_no_ext.ll -o ./output/$filename_no_ext.s \
+  -force-hardware-loops \
+  -force-nested-hardware-loop \
+  -debug \
+  --debug-pass=Executions \
+  -print-after-all
+
+  # -hardware-loop-decrement 1 \
+  # -hardware-loop-counter-bitwidth 16 \
