d10ce232db810ed47e174c2192782b35d017ff22
Jihoon Park <jihoonpark@snu.ac.kr>
2024-10-25 07:37:45 +0000

    implement IMCE Target in lld

---
+++ lld/ELF/Arch/IMCE.cpp
@@ -0,0 +1,73 @@
+//===- IMCE.cpp -----------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Symbols.h"
+#include "Target.h"
+#include "lld/Common/ErrorHandler.h"
+#include "llvm/BinaryFormat/ELF.h"
+#include "llvm/Support/Endian.h"
+
+using namespace llvm;
+using namespace llvm::object;
+using namespace llvm::support::endian;
+using namespace llvm::ELF;
+using namespace lld;
+using namespace lld::elf;
+
+namespace {
+class IMCE final : public TargetInfo {
+public:
+  IMCE();
+  RelExpr getRelExpr(RelType type, const Symbol &s,
+                     const uint8_t *loc) const override;
+  void relocate(uint8_t *loc, const Relocation &rel,
+                uint64_t val) const override;
+};
+} // namespace
+
+IMCE::IMCE() {
+}
+
+RelExpr IMCE::getRelExpr(RelType type, const Symbol &s,
+                           const uint8_t *loc) const {
+  switch (type) {
+  case R_IMCE_NONE:
+    return R_NONE;
+  case R_IMCE_PC6:
+    return R_PC;
+  default:
+    return R_ABS;
+  }
+}
+
+void IMCE::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
+  switch (rel.type) {
+  case R_IMCE_PC6: {
+    // VINN: check this
+    uint32_t mask = 0x00000FC0;
+    checkInt(loc, val, 8, rel);
+    write32(loc, (read32(loc) & ~mask) | ((val << 4) & mask));
+    break;
+  }
+  case R_IMCE_26: {
+    // VINN: check this
+    uint32_t mask = 0xFFFFFFC0;
+    checkIntUInt(loc, val, 26, rel);
+    write32(loc, (read32(loc) & ~mask) | ((val << 4) & mask));
+    break;
+  }
+  default:
+    error(getErrorLocation(loc) + "unrecognized relocation " +
+          toString(rel.type));
+  }
+}
+
+TargetInfo *elf::getIMCETargetInfo() {
+  static IMCE target;
+  return &target;
+}
